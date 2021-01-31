60cb613dcdcb9a7f95e5eafef9b02ed5be006a0b
Jeffery Myers <JeffM2501@gmail.com>
2021-01-31 15:14:10 -0800

    Setup framework

---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "RLGameGui"
+	configurations { "Debug","Debug.DLL", "Release", "Release.DLL" }
+	platforms { "x64"}
+	defaultplatform "x64"
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+		
+	filter "configurations:Debug.DLL"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter "configurations:Release.DLL"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter { "platforms:x64" }
+		architecture "x86_64"
+		
+	targetdir "bin/%{cfg.buildcfg}/"
+		
+project "raylib"
+	filter "configurations:Debug.DLL OR Release.DLL"
+		kind "SharedLib"
+		defines {"BUILD_LIBTYPE_SHARED"}
+		
+	filter "configurations:Debug OR Release"
+		kind "StaticLib"
+		
+	filter "action:vs*"
+		defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+		links {"winmm"}
+				
+	filter{}
+	
+	location "./raylib/"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	cppdialect "C++17"
+	
+	includedirs { "raylib/src", "raylib/src/external/glfw/include"}
+	vpaths 
+	{
+		["Header Files"] = { "raylib/src/**.h"},
+		["Source Files/*"] = {"raylib/src/**.c"},
+	}
+	files {"raylib/src/*.h", "raylib/src/*.c"}
+	
+	defines{"PLATFORM_DESKTOP", "GRAPHICS_API_OPENGL_33"}
+		
+project "RLGameGui"
+	kind "StaticLib"
+		
+	filter "configurations:Debug OR Release"
+		kind "StaticLib"
+		
+	filter "action:vs*"
+		defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+		links {"winmm"}
+				
+	filter{}
+	
+	location "./RLGameGui/"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	cppdialect "C++17"
+	
+	includedirs { "raylib/src"}
+	vpaths 
+	{
+		["Header Files"] = { "RLGameGui/inclue/**.h"},
+		["Source Files"] = {"raylib/**.cpp"},
+	}
+	files {"RLGameGui/**.h", "RLGameGui/**.cpp"}
+
+
+project "test"
+	kind "ConsoleApp"
+	location "%{wks.name}/"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	cppdialect "C++17"
+	
+	vpaths 
+	{
+		["Header Files"] = { "**.h"},
+		["Source Files"] = {"**.c", "**.cpp"},
+	}
+	files {"%{wks.name}/**.c", "%{wks.name}/**.cpp", "%{wks.name}/**.h"}
+
+	links {"raylib","RLGameGui"}
+	
+	includedirs { "%{wks.name}", "raylib/src", "RLGameGui/include" }
+	
+	filter "action:vs*"
+		defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+		links {"winmm"}
